a46c4e1a287ed2a2f2ac40927298076304083051
bytesbanana <saharat.paynok@gmail.com>
2023-01-21 21:32:59 +0700

    Fetch profile from liff

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,60 @@
 <template>
-  <div id="app">Hello World</div>
+  <div className="layout">
+    <img alt="Profile" v-bind:src="profilePic" width="100" height="100" />
+
+    <h1>{{ displayName }}</h1>
+    <p>{{ statusMessage }}</p>
+  </div>
 </template>
 
 <script>
+import liff from '@line/liff';
+
 export default {
   name: 'App',
   components: {},
+  async beforeCreate() {
+    liff.ready.then(async () => {
+      if (liff.isInClient()) {
+        await this.getProfilePic();
+      }
+    });
+    await liff.init({
+      liffId: '1657838953-QPY6AODk',
+    });
+  },
+  methods: {
+    async getProfilePic() {
+      const profile = await liff.getProfile();
+      this.profilePic = profile.pictureUrl;
+      this.displayName = profile.displayName;
+      this.statusMessage = profile.statusMessage;
+    },
+  },
+  data() {
+    return {
+      profilePic: 'https://cdn-icons-png.flaticon.com/512/21/21104.png',
+      displayName: 'Loading...',
+      statusMessage: '',
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.layout {
+  display: flex;
+  border: 5px solid salmon;
+  border-radius: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+}
+img {
+  border-radius: 50%;
+}
+p {
+  color: #808080;
+}
+</style>
